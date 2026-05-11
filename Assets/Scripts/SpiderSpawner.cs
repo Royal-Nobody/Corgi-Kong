cd97ceaff4a14b0e78b8862226a8e3b0acb4ceca
Margaux Reydon <margauxer10@gmail.com>
2026-05-11 12:02:53 -0700

    spider spawner

--- Assets/Scripts/SpiderSpawner.cs
+++ Assets/Scripts/SpiderSpawner.cs
@@ -6,7 +6,6 @@ using UnityEngine.Tilemaps;
 
 public class SpiderSpawner : MonoBehaviour
 {
-    public Tilemap TunnelTilemap;
     public GameObject SpiderPrefabs;
     
     private bool isWaitingtoSpawn = false;
@@ -29,24 +28,6 @@ public class SpiderSpawner : MonoBehaviour
 
     private void Spawn()
     {
-        Vector3 spawnPosition = GetRandomTunnelPosition();
-        
-        Instantiate(SpiderPrefabs, spawnPosition, Quaternion.identity);
-    }
-
-    private Vector3 GetRandomTunnelPosition()
-    {
-        while (true)
-        {
-            Vector3 randomWorldPosition = SpawnTools.RandomLocationWorldSpace();
-            
-            Vector3Int cell = TunnelTilemap.WorldToCell(randomWorldPosition);
-            
-            if (TunnelTilemap.HasTile(cell)) 
-            {
-                return TunnelTilemap.GetCellCenterWorld(cell); 
-            }
-            
-        }
+        Instantiate(SpiderPrefabs, transform.position, Quaternion.identity);
     }
 }
